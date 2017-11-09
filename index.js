191538876a1322aa2e30f2a5074da1a0f91f7a05
Adjoa Darien <adjoa.darien@gmail.com>
2017-11-09 20:31:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+  return customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+  bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'dementors'
+function changeLeastFavoriteCustomer() {
+  leastFavoriteCustomer = 'harry potter';
+}
+
+function attemptTwoFavoriteCustomers() {
+  let favoriteCustomer = 'hermione granger';
+  let favoriteCustomer = 'ron weasley';
+}
